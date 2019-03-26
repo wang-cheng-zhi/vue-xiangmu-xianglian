6b4ba7ad9ded45abd324527b5e8705f174255c1b
SubaiG <1125101660@qq.com>
2019-03-26 11:43:03 +0800

    第一次上传

---
+++ src/components/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <van-tabbar v-model="active" active-color="#fa2c5c">
+      <van-tabbar-item icon="wap-home" :to="{ name: 'Index' }">首页</van-tabbar-item>
+      <van-tabbar-item icon="search" :to="{ name: 'Search' }">发现</van-tabbar-item>
+      <van-tabbar-item icon="shopping-cart-o" :to="{ name: 'Cart' }">购物车</van-tabbar-item>
+      <van-tabbar-item icon="manager-o" :to="{ name: 'MyOne' }">个人中心</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active:2
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/components/Index'
+import MyOne from '@/components/MyOne'
+import Search from '@/components/Search'
+import Cart from '@/components/Cart'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
+    },{
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },{
+      path: '/myone',
+      name: 'MyOne',
+      component: MyOne
+    },{
+      path: '/search',
+      name: 'Search',
+      component: Search
+    },
+  ]
+})
