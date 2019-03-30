81189e06d1677435b925eb134033951c6f36373d
SubaiG <1125101660@qq.com>
2019-03-30 14:02:35 +0800

    order

--- src/components/order/Order_A.vue
+++ src/components/order/Order_A.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+    <h2>暂无订单</h2>
   </div>
 </template>
 
---
+++ src/components/order/Order_B.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>暂无订单</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
